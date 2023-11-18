ffcc57f0e13734449fcf4a11c5b920351cadc02e
haimin <623322116@qq.com>
2023-11-18 23:13:31 +0800

    init project

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: [
+            'haiminovo.cn',
+        ],
+    },
+}
 
 module.exports = nextConfig

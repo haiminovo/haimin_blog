9ebdfc233e3eba30e988e7ec49303f791fd0acb3
haimin <623322116@qq.com>
2024-02-11 23:59:42 +0800

    style changes

---
+++ src/components/articalList/index.tsx
@@ -0,0 +1,67 @@
+import { IArticlesData } from '@/data/mock/articlesMockData';
+import router from 'next/router';
+import Image from 'next/image';
+import style from './index.module.scss';
+
+interface IProps {
+    articlesData: IArticlesData[];
+}
+
+export default function ArticalList({ articlesData }: IProps) {
+    return (
+        <div className={style.articles}>
+            {articlesData.map((item: IArticlesData) => {
+                const tags = item?.seo_keyword?.split(',');
+                return (
+                    <div
+                        className={style.article}
+                        key={item.id}
+                        onClick={() => {
+                            router.push({ pathname: `article/${item.id}` });
+                        }}
+                    >
+                        <div className={style.articleBox}>
+                            <div className={style.articleBox__main}>
+                                <div className={style.articleBox__title}>{item.title}</div>
+                                <div className={style.articleBox__description}>{item.description}</div>
+                                <div className={style.articleBox__infBox}>
+                                    <div className={style.articleBox__inf}>
+                                        <div className={style.articleBox__info}>{item.admin_info?.nickname}</div>
+                                        <div className={style.articleBox__info}>
+                                            <Image src="/imgs/view-fill.svg" alt="" width={16} height={16} />
+                                            {item.browse}
+                                        </div>
+                                        <div className={style.articleBox__info}>
+                                            <Image src="/imgs/follow.svg" alt="" width={16} height={16} />
+                                            {item.favorite_num}
+                                        </div>
+                                    </div>
+
+                                    <div className={style.articleBox__tags}>
+                                        {tags.length > 0 &&
+                                            tags?.map((tag, index) => {
+                                                return (
+                                                    tag && (
+                                                        <span className={style.articleBox__tag} key={index}>
+                                                            {tag}
+                                                        </span>
+                                                    )
+                                                );
+                                            })}
+                                    </div>
+                                </div>
+                            </div>
+                            <Image
+                                className={style.articleBox__img}
+                                width={80}
+                                height={80}
+                                src={item.img_url}
+                                alt={item.title || ''}
+                            />
+                        </div>
+                    </div>
+                );
+            })}
+        </div>
+    );
+}
--- src/pages/article/index.tsx
+++ src/pages/article/index.tsx
@@ -1,10 +1,9 @@
-import style from './article.module.scss';
-import Image from 'next/image';
 import { useEffect, useState } from 'react';
 import Layout from '@/components/layout';
 import { IArticlesData } from '@/data/mock/articlesMockData';
 import { getArticleList } from '@/api/article';
 import { useRouter } from 'next/router';
+import ArticalList from '@/components/articalList';
 
 export default function ArticlesPage() {
     const router = useRouter();
@@ -25,50 +24,7 @@ export default function ArticlesPage() {
 
     return (
         <Layout>
-            <div className={style.articles}>
-                {articlesData.map((item: IArticlesData) => {
-                    return (
-                        <div
-                            className={style.article}
-                            key={item.id}
-                            onClick={() => {
-                                router.push({ pathname: `article/${item.id}` });
-                            }}
-                        >
-                            <div className={style.articleBox}>
-                                <div className={style.articleBox__main}>
-                                    <div className={style.articleBox__title}>{item.title}</div>
-                                    <div className={style.articleBox__description}>{item.description}</div>
-                                    <div className={style.articleBox__infBox}>
-                                        <div className={style.articleBox__inf}>
-                                            <div>{item.admin_info}</div>
-                                            <div>{item.browse}</div>
-                                            <div>{item.favorite_num}</div>
-                                        </div>
-                                        <div className={style.articleBox__tags}>
-                                            {item?.seo_keyword?.split(',').map((tag, index) => {
-                                                return (
-                                                    <span className={style.articleBox__tag} key={index}>
-                                                        {tag}
-                                                    </span>
-                                                );
-                                            })}
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <Image
-                                    className={style.articleBox__img}
-                                    width={80}
-                                    height={80}
-                                    src={item.img_url}
-                                    alt={item.title || ''}
-                                />
-                            </div>
-                        </div>
-                    );
-                })}
-            </div>
+           <ArticalList articlesData={articlesData}></ArticalList>
         </Layout>
     );
 }
